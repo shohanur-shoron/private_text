dccfe93df768f9e52827bdca3849d6d95a460905
Shohanur Rahman Shoron <158572434+shohanur-shoron@users.noreply.github.com>
2024-06-08 14:06:06 +0600

    Update wordCounter.js

--- js/wordCounter.js
+++ js/wordCounter.js
@@ -1,10 +1,13 @@
 function checkInput(inputField) {
     const wordCounter = document.getElementById('wordCounter');
-
+    // Getting the message from the input field and splitting it using space
     var words = inputField.value.trim().split(/\s+/);
-    //console.log(words, isEmptyStringArray(words))
+    // Showing user how many words are already been inputted
     wordCounter.textContent = `Words: ${words.length}/300`;
+    // Increase or decrease the number based on how many words you want to take input
+    // Here the word limit is 300
     if (words.length > 300) {
+        // Replace any character input after 300 word with nothing or ''
         inputField.value = inputField.value.replace(/\s+\S*$/, '');
         inputField.value = inputField.value + ' '
         wordCounter.textContent = `Words: 300/300`;
@@ -14,6 +17,8 @@ function checkInput(inputField) {
     }
 }
 
+//This function will show if the array is empty or not
+// If empty return true otherwise return false
 function isEmptyStringArray(arr) {
     return Array.isArray(arr) && arr.length === 1 && arr[0] === '';
 }
